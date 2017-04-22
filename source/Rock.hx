afefb78ff3d17619f1a249979f6f2a4ff8900168
Simon Weis <laguna.ws@gmail.com>
2017-04-22 20:42:49 +0200

    not using large rocks, small rock variety

--- source/Rock.hx
+++ source/Rock.hx
@@ -19,21 +19,22 @@ class Rock extends FlxSprite
 		
 		//this.makeGraphic(Std.int(GP.TileSize / 2), GP.TileSize, FlxColor.BROWN);
 		
-		var r1 : Bool = FlxG.random.bool();
-		if (r1)
-		{
+		//var r1 : Bool = FlxG.random.bool();
+		//if (r1)
+		//{
 			// small
 			var r2 : Int = FlxG.random.int(0, 7);
 			this.loadGraphic(AssetPaths.Rockset_small__png, true, 16, 16);
 			this.animation.add("idle", [r2], 3, true);
-		}
-		else
-		{
-			var r2 : Int = FlxG.random.int(0, 3);
-			this.loadGraphic(AssetPaths.Rockset_huge__png, true, 32,32);
-			this.animation.add("idle", [r2], 3, true);
-		}
-		
+			this.animation.play("idle");
+		//}
+		//else
+		//{
+			//var r2 : Int = FlxG.random.int(0, 3);
+			//this.loadGraphic(AssetPaths.Rockset_huge__png, true, 32,32);
+			//this.animation.add("idle", [r2], 3, true);
+		//}
+		//
 		this.immovable = true;
 		this.alpha = 0;
 	}
