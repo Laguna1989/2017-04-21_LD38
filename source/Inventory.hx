21cbc70618d988f0ae9d0c3a3f09e01db85beed7
Thunraz <julian.dinges@gmail.com>
2017-04-23 16:18:11 +0200

    Removing traces and old code

--- source/Inventory.hx
+++ source/Inventory.hx
@@ -22,9 +22,7 @@ class Inventory extends FlxTypedGroup<FlxSprite>
 
         Slots = new Array<InventorySlot>();
 
-        var backgroundColor = FlxColor.fromRGB(200, 200, 200, 255);
         _sprBG = new FlxSprite();
-		//_sprBG.makeGraphic(FlxG.width - 20, 80, backgroundColor);
 		_sprBG.loadGraphic(AssetPaths.Itemslots2__png, false, 380, 80);
         _sprBG.scrollFactor.set();
         _sprBG.setPosition(10, FlxG.height);
@@ -70,7 +68,6 @@ class Inventory extends FlxTypedGroup<FlxSprite>
             if(slot.Item.Name == item.Name && slot.Quantity < slot.Item.StackSize)
             {
                 slot.Quantity++;
-                trace(slot.Quantity);
                 return;
             }
         }
