f78f0fc45fe5f2a3c94e1e60522948ed4f4d662d
Thunraz <julian.dinges@gmail.com>
2017-04-23 13:02:25 +0200

    Changed image path to dummy

--- source/tools/StonePickaxe.hx
+++ source/tools/StonePickaxe.hx
@@ -4,6 +4,6 @@ class StonePickaxe extends Tool
 {
     public function new()
     {
-        super("StonePickaxe", "Stone Pickaxe", 1, "assets/images/StonePickaxe.png");
+        super("StonePickaxe", "Stone Pickaxe", 1, "assets/images/dummy.png");
     }
 }
