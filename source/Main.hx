b16043aa69bdd70ee9fbb39cc419a221a98ebcd5
Thunraz <julian.dinges@gmail.com>
2017-04-23 21:41:47 +0200

    Changed first stage to intro

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(400, 300, MenuState, 2, 60, 60, true));
+		addChild(new FlxGame(400, 300, IntroState, 2, 60, 60, true));
 	}
 }
